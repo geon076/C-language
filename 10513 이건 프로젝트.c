3d6d4d542a6cd3e47f0c84e35c41d6b329077529
geon076 <129704478+geon076@users.noreply.github.com>
2023-06-13 12:15:15 +0900

    Update 10513 이건 프로젝트.c
    whwehoTek>>>>

--- 10513 이건 프로젝트.c
+++ 10513 이건 프로젝트.c
@@ -1,8 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-void sudoku();
-void numBaseBall();
+void easy();
+void easyMon();
+void middle();
+void middleMon();
+void hard();
+void hardMon();
+void expert();
+void expertMon();
+void moving();
 int main() {
-	
+
 	return 0;
+}
+void easy() {
+
+}
+void easyMon() {
+
+}
+void middle() {
+
+}
+void middleMon() {
+
+}
+void hard() {
+
+}
+void hardMon() {
+
+}
+void expert() {
+
+}
+void expertMon() {
+
+}
+void moving() {
+
 }
